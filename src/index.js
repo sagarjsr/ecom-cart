de54556f42cfa0561843de28cd8420ca78998dca
sagarjsr <sagargecc@gmail.com>
2021-05-29 19:00:10 +0530

    home login, redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import thunk from 'redux-thunk'
+import AuthReducer from './store/reducers/AuthReducer';
+import * as serviceWorker from "./serviceWorker";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+const RootReducers = combineReducers({
+  category: CategoryReducer,
+  cart: CartReducer,
+  address: AddressReducer,
+  checkout: CheckoutReducer,
+  auth: AuthReducer,
+  order: OrderReducer,
+
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
